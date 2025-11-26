af183809d2744d4dfa501a718bf809175929e5ca
John Hope Dawa <johnhope.dawa@gmail.com>
2025-11-25 17:57:26 -0800

    Update terraform credentials_file path

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+    description = "GCP project ID"
+}
+
+variable "region" {
+    description = "G"
+}
+
+variable "credentials_file" {
+    description = "/home/john/project/Devops-Overengineered-Website/terraform/gcpkey.json"
+}
